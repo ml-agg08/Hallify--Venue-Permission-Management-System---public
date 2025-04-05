13e510cc7d14d933ae4b7850321ca457436e54bc
ml-agg08 <145742905+ml-agg08@users.noreply.github.com>
2025-04-05 22:24:06 +0530

    Update signals.py

--- clubreps/signals.py
+++ clubreps/signals.py
@@ -79,7 +79,6 @@ def send_mail_vfc(sender,instance,title,name,**kwargs):
 
     send_mail(
         subject=title+'''-Permission Approved!''',
-        from_email='anand.gopan08@gmail.com',
         message='''Dear'''+first_name+last_name+''',
         We are pleased to inform you that '''+name+"("+title+")"+''' has approved your venue permission request for the following event details:
 
