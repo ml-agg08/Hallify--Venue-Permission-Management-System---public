22942898ef944e17e3a8bd311e6c23fa6b43bc30
ml-agg08 <145742905+ml-agg08@users.noreply.github.com>
2025-04-05 22:23:49 +0530

    Update signals.py

--- VenueFacultyStaff/signals.py
+++ VenueFacultyStaff/signals.py
@@ -85,7 +85,6 @@ def send_mail_vfc(sender,instance,**kwargs):
         {role}, {club_name}  
         {pm_phone}   
         """,
-        from_email="anand.gopan08@gmail.com",
         recipient_list=vfs.email,
         fail_silently=False,
     )
